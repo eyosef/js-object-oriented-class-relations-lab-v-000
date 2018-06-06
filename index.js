f20eb8d3e87fa1cd30aeaf6cf7ffebc152c808b1
Eden <emesfun@outlook.com>
2018-06-06 12:56:04 -0400

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ class Driver {
     constructor(name) {
         this.name = name; 
         this.id = ++driverId;
-
         store.drivers.push(this);
+    }
+
+
+    trips(){
+        return store.trips.filter(trip => {return this.driverId = this.id});
+    }
+
+    passengers(){
+        let driversPassengers = [];
+
+        this.trips().forEach(t => {
+            driversPassengers.push(store.passengers.find(p => { return p.id === t.passengerId }))
+        });
+        return driversPassengers;
     }
 }
 
@@ -19,23 +32,38 @@ class Passenger {
 
         store.passengers.push(this);
     }
+
+    trips(){
+        return store.trips.filter(trip => {return this.passengerId = this.id});
+    }
+
+    drivers(){
+        let drivers = [];
+
+        this.trips().forEach(t => {
+            drivers.push(store.drivers.find(p => { return p.id === t.driverId }))
+        });
+        return drivers;
+    }
+
 }
+
 
 class Trip {
     constructor(driver, passenger){
-        this.driver = driver;
         this.id = ++tripId;
         this.passengerId = passenger.id;
         this.driverId = driver.id;
+        store.passengers.push(this);
+        this.passenger = this.passenger.bind(this);
         store.trips.push(this);
+    }
 
-        Trip.prototype.passenger = function() {
-            return store.passengers.find(passenger => {return this.passengerId = passenger.id});
-        }
+    passenger(){
+        return store.passengers.find(passenger => {return this.passengerId = passenger.id});
+    }
 
-        Trip.prototype.driver = function() {
-            return store.drivers.find(driver => {return this.driverId = driver.id});
-        }
-
+    driver(){
+        return store.drivers.find(driver => {return this.driverId = driver.id});
     }
 }
